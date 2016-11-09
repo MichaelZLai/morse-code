b218e02c7622c5bd1020aa7e728ba965d5dc3bf3
Michael Lai <mike24567@gmail.com>
2016-11-09 10:42:39 -0500

    solution

--- morse.js
+++ morse.js
@@ -28,6 +28,35 @@ var alphabet = {
   z: '--..'
 }
 module.exports =  {
-  encode: function(){
-  }
-}
+  encode: function(answer){
+    //splits the answer string and makes everything lowercase
+    let characters = answer.toLowerCase().split("")
+    //iterated through the array with map and replaces with the alphabet equivalent
+    let morse = characters.map( letter => {
+      //ternary operator that returns either the alphabet or space
+        //--same as--
+        //   if (alphabet[letter]){
+        //     return alphabet[letter]
+        //   }else{
+        //     return " "
+        //   }
+      return alphabet[letter] ? alphabet[letter] : " "
+    })
+    //joining with a space between the array
+    let morseString = morse.join(" ")
+
+    return morseString
+
+
+      }
+    }
+
+    //////////// FUNCTIONAL WAY OF SOLVING
+    //////////// function(str){
+    ////////////   return str.toLowerCase()
+    ////////////             .split("")
+    ////////////             .map(letter => {
+    ////////////               return alphabet[letter] || " "
+    ////////////             })
+    ////////////             .join(" ")
+    //////////// }
